208d388c604935fb1fe12c5d8fa4577b11d774d2
Vítor Bárrios <vitor.albuquerque@bancointer.com.br>
2021-11-29 16:03:39 -0300

    Initial Commit

---
+++ RequestAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RequestAPI
+//
+//  Created by Vitor Barrios Luis De Albuquerque  on 29/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
